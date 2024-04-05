d3e43c8f8d893c6647e3af95b3b5313f6a2513e8
SokuRitszZ <1062708758@qq.com>
2024-04-05 22:17:06 +0800

    fix: miss an 's'

--- plugins/vite-plugin-redirect-gen.ts
+++ plugins/vite-plugin-redirect-gen.ts
@@ -1,6 +1,6 @@
-import { Plugin } from 'vite';
 import fs from 'node:fs';
 import path from 'node:path';
+import { Plugin } from 'vite';
 
 export const redirectGen = (): Plugin => {
   return {
@@ -9,11 +9,11 @@ export const redirectGen = (): Plugin => {
     writeBundle: async () => {
       console.log('\nGenerating netlify redirect file...');
 
-      const content = `/*    /index.html   200`;
-      const outputPath = path.resolve(process.cwd(), 'dist', '_redirect');
+      const content = '/*    /index.html   200';
+      const outputPath = path.resolve(process.cwd(), 'dist', '_redirects');
       fs.writeFileSync(outputPath, content, {});
 
       console.log('Generated file successfully!');
-    }
+    },
   };
-}
+};
