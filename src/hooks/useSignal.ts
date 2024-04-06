dd94e3ced9d5718b75f2617bbfcfb08d2d23f74a
SokuRitszZ <1062708758@qq.com>
2024-04-06 21:37:54 +0800

    fix: replace useSignal

--- src/hooks/useSignal.ts
+++
@@ -1,9 +0,0 @@
-import { createSignal } from 'solid-js';
-
-export const useSignal = <T>(defaultValue?: T) => {
-  const [signal, setSignal] = createSignal<T>(defaultValue as T);
-  return {
-    v: signal,
-    s: setSignal,
-  };
-};
